81d50094c4632f9427e9a6b15ab78453ba0584d9
t2831245 <s11113244@stu.edu.tw>
2023-01-20 15:47:11 +0800

    add jwt validation

---
+++ src/main/java/com/demo/entity/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+
+}
